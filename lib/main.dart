7221d1363135ab4a031d4e07c7f008a9011c3db8
dafma <mar_tp@hotmail.es>
2020-10-16 11:22:38 -0500

    game discovery

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:dreamwelker/src/game_discovery_601.dart';
 import 'package:dreamwelker/src/rental_585.dart';
 import 'package:dreamwelker/travel_app_ep498-499.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Rental_585());
+  runApp(GameDiscovery());
 }
---
+++ lib/src/game_discovery_601.dart
@@ -0,0 +1,300 @@
+import 'package:flutter/material.dart';
+
+import 'package:badges/badges.dart';
+
+class GameTab {
+  String title;
+  bool isSelected;
+
+  GameTab(this.title, this.isSelected);
+}
+
+class GameDiscovery extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: GameDiscoveryHomePage(),
+    );
+  }
+}
+
+class GameDiscoveryHomePage extends StatefulWidget {
+  GameDiscoveryHomePage({Key key}) : super(key: key);
+
+  @override
+  _GameDiscoveryHomePageState createState() => _GameDiscoveryHomePageState();
+}
+
+class _GameDiscoveryHomePageState extends State<GameDiscoveryHomePage> {
+  List<String> _tabList = ["AR Games", "Adventure", "Sports", "Arcade"];
+  int _tabIndex = 0;
+  List<GameTab> _tabListV2 = [
+    GameTab("AR Games", true),
+    GameTab("Adventure", false),
+    GameTab("Sports", false),
+    GameTab("Arcade", false),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomPadding: false,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Container(
+        height: 64,
+        width: 64,
+        decoration: BoxDecoration(
+            color: Colors.indigo,
+            shape: BoxShape.circle,
+            border: Border.all(color: Colors.white, width: 4)),
+        child: Center(
+          child: Icon(
+            Icons.add,
+            size: 38,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 64,
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(icon: Icon(Icons.home), onPressed: () {}),
+              IconButton(icon: Icon(Icons.personal_video), onPressed: () {}),
+              IconButton(icon: Icon(Icons.person_outlined), onPressed: () {}),
+              IconButton(icon: Icon(Icons.person_outline), onPressed: () {}),
+            ],
+          ),
+        ),
+        shape: CircularNotchedRectangle(),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 40, left: 16, right: 16),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 6,
+                    child: Container(
+                      height: 48,
+                      decoration: BoxDecoration(
+                          color: Colors.indigo[50],
+                          borderRadius: BorderRadius.circular(24)),
+                      padding:
+                          EdgeInsets.only(left: 8, right: 8, top: 4, bottom: 4),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 8,
+                            child: TextField(
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                              decoration:
+                                  InputDecoration(border: InputBorder.none),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: CircleAvatar(
+                              backgroundColor: Colors.indigo,
+                              child: Icon(Icons.search),
+                              foregroundColor: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                      flex: 1,
+                      child: Center(
+                        child: Badge(
+                          child: Icon(Icons.notifications_active_outlined),
+                          badgeContent: Text(
+                            "12",
+                            style: TextStyle(fontSize: 10, color: Colors.white),
+                          ),
+                          borderRadius: 3,
+                        ),
+                      ))
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 26, top: 16),
+              height: MediaQuery.of(context).size.height / 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Text(
+                      "Arena",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 8,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: RotatedBox(
+                            quarterTurns: -1,
+                            child: Text(
+                              "Global + Multi-Language",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.grey),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 9,
+                          child: ListView(
+                            scrollDirection: Axis.horizontal,
+                            children: [
+                              Container(
+                                margin: EdgeInsets.all(8),
+                                width: MediaQuery.of(context).size.width / 1.8,
+                                decoration: BoxDecoration(
+                                    color: Colors.blue,
+                                    borderRadius: BorderRadius.circular(16),
+                                    image: DecorationImage(
+                                        image: NetworkImage(
+                                          "https://cdn.pixabay.com/photo/2015/02/07/13/36/computer-627220_960_720.jpg",
+                                        ),
+                                        fit: BoxFit.cover)),
+                                padding: EdgeInsets.all(16),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Spacer(),
+                                    Text(
+                                      "GANE TITLE 01",
+                                      style: TextStyle(
+                                          fontSize: 24, color: Colors.white),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.all(8),
+                                width: MediaQuery.of(context).size.width / 1.8,
+                                decoration: BoxDecoration(
+                                    color: Colors.blue,
+                                    borderRadius: BorderRadius.circular(16),
+                                    image: DecorationImage(
+                                        image: NetworkImage(
+                                          "https://cdn.pixabay.com/photo/2015/02/07/13/36/computer-627220_960_720.jpg",
+                                        ),
+                                        fit: BoxFit.cover)),
+                                padding: EdgeInsets.all(16),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Spacer(),
+                                    Text(
+                                      "GAME TITLE 02",
+                                      style: TextStyle(
+                                          fontSize: 24, color: Colors.white),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                "Categories",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              height: 48,
+              decoration: BoxDecoration(),
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: _tabListV2.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: GestureDetector(
+                      onTap: () {
+                        _tabListV2[_tabIndex].isSelected = false;
+                        setState(() {
+                          _tabListV2[index].isSelected = true;
+                          _tabIndex = index;
+                        });
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 24),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(24),
+                            border: _tabListV2[index].isSelected
+                                ? Border.all(color: Colors.indigo, width: 2)
+                                : null),
+                        child: Center(
+                          child: Text(
+                            _tabListV2[index].title,
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: _tabListV2[index].isSelected
+                                    ? FontWeight.bold
+                                    : FontWeight.normal),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 24),
+              child: Container(
+                margin: EdgeInsets.only(left: 16, top: 16),
+                height: MediaQuery.of(context).size.height / 4,
+                child: ListView.builder(
+                    itemCount: 9,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        margin: EdgeInsets.only(right: 16),
+                        width: MediaQuery.of(context).size.width / 3,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(16),
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                  "https://cdn.pixabay.com/photo/2015/02/07/13/36/computer-627220_960_720.jpg",
+                                ),
+                                fit: BoxFit.cover)),
+                      );
+                    }),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
